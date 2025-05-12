13d11d228d8ad667ce3de9b7b5dfec0b4ed14bd8
Slavonator <hedoima.polzovatela@gmail.com>
2025-05-12 21:38:24 +0300

    обновил game_map.py

--- map_related_files/game_map.py
+++ map_related_files/game_map.py
@@ -29,38 +29,43 @@ class GameMap:
         self.x_indent, self.y_indent = 0, 0
         self.max_x, self.max_y, self.min_x, self.min_y = 0, 0, 0, 0
     
+    def __str__(self):
+        return str(self.game_map)
+    
+    def __iter__(self):
+        return iter(self.game_map)
+    
     def add_sector(self, sector: Sector):
         sector_x, sector_y = sector.get_position()
         if not (self.min_x <= sector_x <= self.max_x and self.min_y <= sector_y <= self.max_y):
             return
-        if self.min_x == self.max_x == sector_x == 0 and self.min_y == self.max_y == sector_y == 0:
-            self.min_x -= 1
-            self.max_x += 1
-            self.min_y -= 1
-            self.max_y += 1
         # else:
         #     if self.min_x == sector_x
     
     def expand_map(self, direction):
         for symbol in direction:
             if symbol == 'n':
-                self.game_map.insert(0, [None] * self.game_map_height)
+                self.game_map.insert(0, ['n'] * self.game_map_width)
+                self.max_y += 1
             if symbol == 's':
-                self.game_map.insert(-1, [None] * self.game_map_height)
+                self.game_map.append(['s'] * self.game_map_width)
+                self.min_y -= 1
             if symbol == 'e':
-                for row in range(self.game_map_width):
-                    self.game_map[row].insert(-1, None)
+                for row in range(self.game_map_height):
+                    self.game_map[row].append('e')
+                self.max_x += 1
             if symbol == 'w':
-                for row in range(self.game_map_width):
-                    self.game_map[row].insert(0, None)
+                for row in range(self.game_map_height):
+                    self.game_map[row].insert(0, 'w')
+                self.min_x -= 1
     
     @property
     def game_map_width(self):
-        return self.max_x - self.min_x
+            return self.max_x - self.min_x + 1
     
     @property
     def game_map_height(self):
-        return self.max_y - self.min_y
+        return self.max_y - self.min_y + 1
     
 
 if __name__ == '__main__':
@@ -68,11 +73,5 @@ if __name__ == '__main__':
     MAP_GENERATOR = MapGenerator()
     sector = Sector(MAP_GENERATOR.make_clear_map(), 0, 0)
     game_map = GameMap()
-    game_map.expand_map('n')
-    print(game_map.game_map, 'n')
-    game_map.expand_map('s')
-    print(game_map.game_map, 's')
-    game_map.expand_map('e')
-    print(game_map.game_map, 'e')
-    game_map.expand_map('w')
-    print(game_map.game_map)
+    for row in game_map:
+        print(row)
