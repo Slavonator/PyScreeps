5859adc49e0c859e8db4b4b11e327089e16cbd09
Slavonator <hedoima.polzovatela@gmail.com>
2025-05-16 15:05:16 +0300

    начало работы над интерфейсом для того, что есть

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ from textual.widgets import Header, Footer, Static
 GENERATOR = map_generator.MapGenerator()
 GLOBAL_MAP = game_map.GameMap()
 
+class MapSection(Static, can_focus=True):
+
+    def on_mount(self):
+        self.update("foobar")
+    
+    def on_key(self, event):
+        self.update(event.key)
+
 class Interface(App):
 
     def compose(self):
         yield Header()
+        yield MapSection()
         yield Footer()
 
 if __name__ == '__main__':
